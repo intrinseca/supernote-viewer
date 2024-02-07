46d8d7722a16fa01154ab60cb082a021aebf0e91
Michael Allan <michael@intrinseca.me.uk>
2024-02-07 12:57:37 +0000

    Make start and restart automatic

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import asyncio
 import ctypes
 import tkinter as tk
 from io import BytesIO
@@ -10,31 +11,45 @@ import toml
 config = toml.load("settings.toml")
 
 
+async def stream_from(resp):
+    reader = aiohttp.MultipartReader.from_response(resp)
+    while True:
+        try:
+            part = await reader.next()
+            partdata = await part.read(decode=False)
+            print("Got part")
+        except (asyncio.exceptions.TimeoutError, aiohttp.client_exceptions.ClientPayloadError):
+            label.config(text="Timed out...", image="")
+            break
+
+        partdatastream = BytesIO(partdata)
+
+        pil_image = Image.open(partdatastream)
+
+        label.update()
+        pil_image.thumbnail((label.winfo_width(), label.winfo_height()))
+
+        image = ImageTk.PhotoImage(pil_image)
+        label.image = image
+        label.config(image=image, text="")
+
 async def load_image():
-    button.config(state=tk.DISABLED)
     label.config(text="Loading...", image="")
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(
-            f"http://{config['supernote_address']}:8080/screencast.mjpeg"
-        ) as resp:
-            reader = aiohttp.MultipartReader.from_response(resp)
-            while True:
-                part = await reader.next()
-                partdata = await part.read(decode=False)
-                print(f"Got Part of {len(partdata)} bytes")
+    timeout = aiohttp.ClientTimeout(connect=2, total=60)
 
-                partdatastream = BytesIO(partdata)
-
-                pil_image = Image.open(partdatastream)
-
-                label.update()
-                pil_image.thumbnail((label.winfo_width(), label.winfo_height()))
-
-                image = ImageTk.PhotoImage(pil_image)
-                label.image = image
-                label.config(image=image, text="")
-                button.config(state=tk.NORMAL)
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+        while True:
+            print("Connecting...")
+            try:
+                async with session.get(
+                    f"http://{config['supernote_address']}:8080/screencast.mjpeg"
+                ) as resp:
+                    await stream_from(resp)
+            except aiohttp.client_exceptions.ServerTimeoutError:
+                label.config(text="Timed out...", image="")
+                continue
+                
 
 
 if __name__ == "__main__":
@@ -50,10 +65,12 @@ if __name__ == "__main__":
     photo = ImageTk.PhotoImage(ico)
     root.wm_iconphoto(False, photo)
 
-    button = tk.Button(root, text="Load an image", command=async_handler(load_image))
-    button.pack()
+    # button = tk.Button(root, text="Start", command=async_handler(load_image))
+    # button.pack()
 
     label = tk.Label(root)
     label.pack(expand=1, fill=tk.BOTH)
+
+    asyncio.get_event_loop_policy().get_event_loop().create_task(load_image())
 
     async_mainloop(root)
